07ef17f03209e4dbde506e489ff44e5a2c28d500
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-09-24 23:58:59 +0530

    Zingtouch use

--- src/Controls.js
+++ src/Controls.js
@@ -4,16 +4,23 @@ import ZingTouch from 'zingtouch';
 
 const Controls = () => {
 
+    let rotate = useRef('rotatable')
+    let target = useRef('touch-sense');
+    let currentAngle = 15;
     const touch = () => {
-        let target = useRef('touch-sense');
+
         let region = new ZingTouch.Region(target);
+        region.bind(target, 'rotate', function (e) {
+
+            currentAngle += e.detail.distanceFromLast;
+        })
     }
 
     return (
 
         <div className="main-div" ref="touch-sense">
             <div className="menu"> <span>MENU</span></div>
-            <div className="center">
+            <div className="center" ref="rotatable">
                 <div className="backward-div"><i class="fa fa-fast-backward" aria-hidden="true"></i></div>
                 <div className="center-div"></div>
                 <div className="forward-div"><i class="fa fa-fast-forward" aria-hidden="true"></i></div>
